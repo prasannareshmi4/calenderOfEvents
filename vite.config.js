9665484cdaa893eceb53b7c60ebeb638d3ba620c
prasannareshmi4 <prasannareshmi4@gmail.com>
2025-09-24 21:05:10 +0530

    added vercel.json for routing fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-      base: "/Calender_of_events_generator/",
+      base: "./",
       plugins: [react()],
 });
