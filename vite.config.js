4af9e1dfa088761b34b33de5645d26f1ea29ae51
prasannareshmi4 <prasannareshmi4@gmail.com>
2025-09-24 21:09:28 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//       base: "./",
+//       plugins: [react()],
+// });
+
+
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-      base: "./",
-      plugins: [react()],
+  base: './',   // 👈 Add this line
+  build: {
+    outDir: 'dist'
+  }
 });
